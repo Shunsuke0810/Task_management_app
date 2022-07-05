b915cbc0a318389204cd700dacef7364604da5be
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-06 08:41:25 +0900

    カラムに締切日時'period'を設定しviewwo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_022250) do
+ActiveRecord::Schema.define(version: 2022_07_05_091609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_06_30_022250) do
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "period"
   end
 
 end
