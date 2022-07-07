79e95a95da01d85668ae744accc3e4c6f52afd5f
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-07 18:05:22 +0900

    タスクのステータス機能を実装しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :content, :period)
+      params.require(:task).permit(:title, :content, :period, :status)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :period, presence: true
   scope :latest, -> {order(updated_at: :desc)}
+  enum status: {未着手:1,着手中:2,完了:3}
 end
