a943447c06ef875f4759255794d744df0dcbec1a
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-11 09:45:42 +0900

    優先順位を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -78,6 +78,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :content, :period, :status)
+      params.require(:task).permit(:title, :content, :period, :status, :priority)
     end
 end
