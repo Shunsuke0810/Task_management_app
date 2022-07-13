678659b2a0d715b223d790e83f2b9a08b92ed40d
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-13 14:45:22 +0900

    usersテーブルとログインシステムを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks 
+  resources :users, only: [:new, :create, :show]
 end
