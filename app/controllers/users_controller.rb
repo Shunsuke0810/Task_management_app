bc0e78a06e0e16f4b84b762b50b441d79221ca51
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-14 08:58:07 +0900

    ログイン状態の時,new画面に移動しないよう実装、current_user以外のユーザーのマイページアクセス時タスク一覧に遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,26 @@ class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
 
   def show
+    if current_user == User.find(params[:id])
     @user = User.find(params[:id])
+    else
+      redirect_to tasks_path
+    end
   end
 
   def new
+    if current_user.present?
+      redirect_to tasks_path
+    else
     @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user.id)
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       render :new
     end
