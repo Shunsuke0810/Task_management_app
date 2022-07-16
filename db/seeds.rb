4e6c478533874c8faf270d9ac0151fd2736dc48f
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-16 11:06:07 +0900

    seed fileでタスク、ユーザーのシードデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,53 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(
+  id: 1,
   name: "admin", 
-  email: "admin@gadmin.com", 
+  email: "admin@admin.com", 
   password: "aaaaaa",
   password_confirmation: "aaaaaa",
   admin: true
 )
 
 User.create(
-  name: "sato", 
-  email: "sato@gmail.com", 
+  id: 2,
+  name: "a", 
+  email: "a@a.com", 
   password: "aaaaaa",
   password_confirmation: "aaaaaa",
   admin: false
 )
+
+User.create(
+  id: 3,
+  name: "b", 
+  email: "b@b.com", 
+  password: "bbbbbb",
+  password_confirmation: "bbbbbb",
+  admin: false
+)
+
+Task.create(
+  title: "aのタスク",
+  content: "a",
+  created_at: "2022-07-16",
+  updated_at: "2022-07-16",
+  period: "2022-07-19",
+  status: 1,
+  priority: 1,
+  user_id: 2
+)
+
+Task.create(
+  title: "bのタスク",
+  content: "b",
+  created_at: "2022-07-17",
+  updated_at: "2022-07-17",
+  period: "2022-07-20",
+  status: 2,
+  priority: 2,
+  user_id: 3
+)
+
+
 
