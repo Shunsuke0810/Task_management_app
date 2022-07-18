50764f91808c918f6aa3936e44bf6a8627d60547
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-19 08:50:02 +0900

    ラベル実装のテストを実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :task do
+  factory :task, :class => 'task' do
+    id {1}
+    user_id { 1 }
     title { 'A' }
     content { 'B' }
     period { Date.today }
@@ -7,3 +9,28 @@ FactoryBot.define do
     priority { '高' }
   end
 end
+
+FactoryBot.define do
+  factory :label_ruby, :class => 'label' do
+    name {'Ruby'}
+    created_at { Date.today }
+    updated_at { Date.today }
+  end
+end
+
+FactoryBot.define do
+  factory :label_php, :class => 'label' do
+    name {'Php'}
+    created_at { Date.today }
+    updated_at { Date.today }
+  end
+end
+
+FactoryBot.define do
+  factory :label_python, :class => 'label' do
+    name {'Python'}
+    created_at { Date.today }
+    updated_at { Date.today }
+  end
+end
+
