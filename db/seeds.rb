86aba8fa21fe6ce80d01d54bb5d0fec7cd96ea39
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-19 11:02:59 +0900

    user,task,labelの各10件分のseedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,73 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+10.times do |n|
 User.create(
-  id: 1,
-  name: "admin", 
-  email: "admin@admin.com", 
-  password: "aaaaaa",
-  password_confirmation: "aaaaaa",
-  admin: true
-)
-
-User.create(
-  id: 2,
-  name: "a", 
-  email: "a@a.com", 
+  id: n + 2,
+  name: "name#{n + 1}",
+  email: "#{n + 1}@gmail.com",
   password: "aaaaaa",
   password_confirmation: "aaaaaa",
   admin: false
 )
+end
 
-User.create(
-  id: 3,
-  name: "b", 
-  email: "b@b.com", 
-  password: "bbbbbb",
-  password_confirmation: "bbbbbb",
-  admin: false
-)
-
+10.times do |n|
 Task.create(
-  title: "aのタスク",
-  content: "a",
+  id: n + 1,
+  title: "#{n + 1}",
+  content: "task",
   created_at: "2022-07-16",
   updated_at: "2022-07-16",
   period: "2022-07-19",
   status: 1,
   priority: 1,
-  user_id: 2
+  user_id: n + 1
 )
+end
 
-Task.create(
-  title: "bのタスク",
-  content: "b",
-  created_at: "2022-07-17",
-  updated_at: "2022-07-17",
-  period: "2022-07-20",
-  status: 2,
-  priority: 2,
-  user_id: 3
-)
-
+10.times do |n|
 Label.create(
-  name: "Ruby",
+  name: "Label_#{n + 1}",
   created_at: "2022-07-17",
   updated_at: "2022-07-17",
 )
-
-Label.create(
-  name: "PHP",
-  created_at: "2022-07-17",
-  updated_at: "2022-07-17",
-)
-
-Label.create(
-  name: "Python",
-  created_at: "2022-07-17",
-  updated_at: "2022-07-17",
-)
-
+end
 
 
 
